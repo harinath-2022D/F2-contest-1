9d0445915205f4673b649e406c7761d9f5fc0708
Harinath <harireddyakkati.91@gmail.com>
2023-08-12 16:02:46 +0530

    js code done

--- script.js
+++ script.js
@@ -9,18 +9,40 @@ let arr = [
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+  for(let i=0; i<arr.length; i++){
+    let obj = arr[i];
+    if(obj.profession==='developer')
+       console.log(arr[i]);
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let obj = {id:4,name:"susan",age:20,profession:"intern"};
+  arr.push(obj);
+  console.log(obj);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  for(let i=0; i<arr.length; i++){
+    let obj = arr[i];
+    if(obj.profession==='admin'){
+      arr.splice(i,1);
+    }
+  }
+
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [{id:5,name:"Div",age:22,profession:"JSD"},
+              {id:6,name:"Accio",age:50,profession:"mentor"}
+             ];
+  let concatedArray = [...arr, ...arr1];
+  arr = concatedArray;
+  console.log(arr);
 }
 
 // Here is an example of how functions work,
